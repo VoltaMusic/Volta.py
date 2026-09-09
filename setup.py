7200cfcf509ba914778be45b211b1dc2ba8d7394
Hugo Hennetin <hugo.hennetin@proton.me>
2026-09-09 21:38:42 +0200

    BUMP: Update version to 0.6.2 in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="VoltaLib",
-	version="0.6.1",
+	version="0.6.2",
 	description="VoltaLibPython packaged as VoltaLib",
 	packages=find_packages(exclude=("tests", "docs")),
 	include_package_data=True,
